e24b61fc63587ce14ce5db3656af3726ffd5b51a
Vladimir Semchenkov <semchenkovvova@gmail.com>
2020-10-10 23:50:28 +0300

    Ajax --  v. 0.1

---
+++ js/asynchronous/callbacks.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+
+    let http = new XMLHttpRequest();
+
+
+    http.onreadystatechange = () => {
+        if(http.readyState == 4 && http.status == 200) {
+            console.log(JSON.parse(http.response));
+        }
+    }
+    http.open("GET", "js/asynchronous/data/movies.json", true)
+    http.send()
+
+
+    $.get("/js/asynchronous/data/names.json",
+        function (data) {
+            console.log(data)
+        }
+    );
+}
+
+/*
+
+*/
