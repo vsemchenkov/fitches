65d723657ffabdede9949b3f27a0486cd8fab848
Vladimir Semchenkov <semchenkovvova@gmail.com>
2020-10-11 16:02:20 +0300

    Generator Wrap

---
+++ js/asynchronous/generators.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+
+    genWrap(function* () {
+        let movies = yield $.get('/js/asynchronous/data/movies.json');
+        console.log(movies);
+        let names = yield $.get('/js/asynchronous/data/names.json');
+        console.log(names);
+    })
+
+    function genWrap(generator) {
+        gen = generator();
+
+        function handle(yielded) {
+            if (!yielded.done) {
+                yielded.value.then((data) => {
+                    return handle(gen.next(data));
+                })
+            }
+        }
+
+        return handle(gen.next())
+    }
+}
